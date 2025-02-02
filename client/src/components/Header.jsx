b3c7bf33597dc6f9b2eaa3e6114dc97e5362e507
Mohamed Belal <mohamed.belal@theapsgroup.com>
2025-02-02 05:25:32 +0100

    feat: get the logo from s3 bucket

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,14 @@
-import logo from "./assets/logo.png";
-
 export default function Header() {
   return (
     <nav className="navbar bg-light mb-4 p-0">
       <div className="container">
         <a className="navbar-brand" href="/">
           <div className="d-flex">
-            <img src={logo} alrt="logo" className="mr-2" />
+            <img
+              src="https://project-manager-bucket-test.s3.eu-central-1.amazonaws.com/logo.png"
+              alt="logo"
+              className="mr-2"
+            />
             <div>project manager</div>
           </div>
         </a>
